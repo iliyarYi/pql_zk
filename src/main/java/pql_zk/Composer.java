b78efda6796a74e7bbab32663abcb669445e3b6f
Yiliyaer <y.yilihamu@leonardo.com.au>
2018-12-12 15:02:36 +1100

    working version of PQL with zk frontend

--- src/main/java/pql_zk/Composer.java
+++ src/main/java/pql_zk/Composer.java
@@ -14,6 +14,7 @@ import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Hlayout;
+import org.zkoss.zul.Label;
 import org.zkoss.zul.Textbox;
 import org.pql.query.*;
 import java.util.*;
@@ -25,80 +26,73 @@ import org.antlr.v4.runtime.*;
 public class Composer extends GenericForwardComposer {
 
     private static PQLAPI pqlAPI = null;
-
-    Map<String, String> myMap = new HashMap();
+    PQLQueryResult queryResult = null;
     Hlayout container;
+    // Textbox (ace editor) ID in zul
     Textbox aceTextBox;
+    // Result Label ID in zul
+    Label resultLabel;
+
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
 
     }
-
+    // Function to be executed when OK button is clicked
     public void onClickOk(Event event) throws Exception{
-
-
+        // Get value entered in ace editor
         String textValue = aceTextBox.getValue();
-        PQLQueryResult queryResult = null;
-        StringBuilder returnedResult = new StringBuilder();
-
-        MySQLConnection mysql = new MySQLConnection("jdbc:mysql://localhost:3306/pql", "root", "password");
-
-        Set<Double> indexedLabelSimilarities = null;
-        LabelManagerType labelManagerType = null;
-        PQLIniFile iniFile = new PQLIniFile();
+        // Ignore empty spaces and lines
         textValue = textValue.replaceAll("\\s+","");
-
-
-
-        indexedLabelSimilarities = new HashSet<Double>();
-        indexedLabelSimilarities.add(0.5);
-        indexedLabelSimilarities.add(0.75);
-        indexedLabelSimilarities.add(1.0);
-
-        labelManagerType = LabelManagerType.LUCENE;
-
-
-//        ILabelManager labelMngr = labelMngr = new LabelManagerLuceneVSM(
-//                mysql.getConnection(),
-//                0.75, indexedLabelSimilarities, ""
-//        );
-
-
-//        System.out.println("Label: " + labelManagerType.toString() + " And indexedLabel: " + indexedLabelSimilarities);
-
+        // Load PQL.ini file
+        PQLIniFile iniFile = new PQLIniFile();
         if (!iniFile.load()) {
-            //iniFile.create();
+            iniFile.create();
             if (!iniFile.load()) {
                 System.out.println("ERROR: Cannot load PQL ini file.");
                 return;
             }
         }
-
+        // Set up pqlAPI using PQL-Tool.jar with configurations in PQL.ini file
+        pqlAPI = new PQLAPI(iniFile.getMySQLURL(), iniFile.getMySQLUser(), iniFile.getMySQLPassword(), iniFile.getPostgreSQLHost(), iniFile.getPostgreSQLName(), iniFile.getPostgreSQLUser(), iniFile.getPostgreSQLPassword(), iniFile.getLoLA2Path(), iniFile.getLabelSimilaritySeacrhConfiguration(), iniFile.getIndexType(), iniFile.getLabelManagerType(), iniFile.getDefaultLabelSimilarityThreshold(), iniFile.getIndexedLabelSimilarityThresholds(), iniFile.getNumberOfQueryThreads(), (long)iniFile.getDefaultBotMaxIndexTime(), (long)iniFile.getDefaultBotSleepTime());
+        // Convert input textValue to token that can be used by Antlr parser.
         CharStream stream = new ANTLRInputStream(textValue);
         PQLLexer lexer = new PQLLexer(stream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         PQLParser parser = new PQLParser(tokens);
-
+        // Remove console error message
         parser.removeErrorListeners();
         parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 
-        System.out.println(iniFile.getMySQLURL());
-        queryResult = pqlAPI.query(textValue);
+        // Debug: Show results in console
         System.out.println("Result:\t\t" + queryResult.getSearchResults());
+
         try {
+            // Parse input query using antlr parser
             parser.query();
-//            queryResult = new PQLQueryResult(2, "jdbc:mysql://localhost:3306/pql", "root", "password", textValue, labelMngr, "localhost", "themis", "user", "password", "./lucene", 0.75, indexedLabelSimilarities,
-//            labelManagerType);
-            Clients.showNotification("Query Ok!", "info", null, null, 5000);
+            // Assign result of the query into queryResult
+            queryResult = pqlAPI.query(textValue);
+            // Show notification to user
+            Clients.showNotification("Query Ok!", "info", null, null, 2000);
 //            System.out.println(queryResult.getSearchResults());
         } catch (Exception e) {
+            //Get error message and show it to user as error notification when bad things happen
             String error = e.getMessage();
             System.out.println("Error!"+e);
             Clients.showNotification("Syntax Error! " + error, "error", null, null, 5000);
         }
 
+        // Set the result label value to show in frontend
+        // resultLabel ID is set in .zul
+        resultLabel.setValue(getResult());
+    }
+
+    public String getResult() {
+        // Function to convert Sets to String so it can be printed on frontend
+        System.out.println(String.join(",",queryResult.getSearchResults()));
+        return String.join(",",queryResult.getSearchResults());
     }
     public void onClickCancel(Event event) throws Exception{
+        // Happens when clicked cancel button
         Clients.showNotification("Clicked Cancel button","info",null,null,5000);
     }
 
